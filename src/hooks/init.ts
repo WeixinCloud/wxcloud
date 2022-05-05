a412614a7afadbcf3f226bb075d032a7f4b978b0
haotianlong <haotianlong@tencent.com>
2022-05-05 14:20:11 +0800

    fix: init error

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,12 +1,15 @@
+import { Hook } from "@oclif/core";
+import { initCloudAPI } from "../api/adapter";
+import { readLoginState } from "../utils/auth";
+import { logger } from "../utils/log";
 
-import { Hook } from '@oclif/core'
-import { initCloudAPI } from '../api/adapter'
-import { tcbDescribeEnvLimit } from '../api/cloudapi';
-import { readLoginState } from '../utils/auth';
+const hook: Hook<"init"> = async function (options) {
+  try {
+    const appid = (await readLoginState()).appid;
+    initCloudAPI(appid);
+  } catch (error) {
+    logger.debug("init cloudapi: ", error);
+  }
+};
 
-const hook: Hook<'init'> = async function (options) {
-  const appid = (await readLoginState()).appid;
-  initCloudAPI(appid)
-}
-
-export default hook
+export default hook;
