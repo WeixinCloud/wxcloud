05ccc8d8076f74b99923df5b01d724c02e251b62
haotianlong <haotianlong@tencent.com>
2022-05-10 11:56:42 +0800

    upd

--- packages/cloudkit/scripts/build.ts
+++ packages/cloudkit/scripts/build.ts
@@ -13,14 +13,14 @@ const baseConfig = {
 async function main() {
   await esbuild({
     ...baseConfig,
-    outdir: path.join(__dirname, "../build/cjs"),
+    outdir: path.join(__dirname, "../lib/cjs"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
   });
 
   await esbuild({
     ...baseConfig,
     format: "esm",
-    outdir: path.join(__dirname, "../build/esm"),
+    outdir: path.join(__dirname, "../lib/esm"),
     entryPoints: [path.join(__dirname, "../src/index.ts")],
   });
 }
