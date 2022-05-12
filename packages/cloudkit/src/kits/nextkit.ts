3aac5f81187f8d7c5044615c3bbb724334962191
haotianlong <haotianlong@tencent.com>
2022-05-12 17:17:04 +0800

    fix(nextkit): use custom dockerfile for deployment

--- packages/cloudkit/src/kits/nextkit.ts
+++ packages/cloudkit/src/kits/nextkit.ts
@@ -1,36 +1,31 @@
-import { exec } from "node:child_process";
-import { existsSync, readFileSync, unlinkSync, writeFileSync } from "node:fs";
-import path from "node:path";
-import { promisify } from "node:util";
-import { IKitContext, IKitDeployTarget, Kit, KitType } from "../common/kit";
-import { RunKit } from "./runkit";
+import { exec } from 'node:child_process';
+import { existsSync, readFileSync, unlinkSync, writeFileSync } from 'node:fs';
+import path from 'node:path';
+import { promisify } from 'node:util';
+import { IKitContext, IKitDeployTarget, Kit, KitType } from '../common/kit';
+import { RunKit } from './runkit';
 
 export class NextKit extends Kit {
-  static description = "CloudKit for Next.js";
+  static description = 'CloudKit for Next.js';
   static type = KitType.UNIVERSAL;
   detect(ctx: IKitContext): boolean | Promise<boolean> {
-    const packageJson = require(path.join(ctx.fullPath, "package.json"));
+    const packageJson = require(path.join(ctx.fullPath, 'package.json'));
+    console.log('nextkit::detect', packageJson);
     return !!packageJson.dependencies.next;
   }
   async run(ctx: IKitContext): Promise<IKitDeployTarget> {
     // patch next.config.js
-    const nextConfigPath = path.join(ctx.fullPath, "next.config.js");
+    const nextConfigPath = path.join(ctx.fullPath, 'next.config.js');
     const nextConfig = require(nextConfigPath);
     if (!ctx.staticDomain) {
-      throw new Error("staticDomain is required using NextKit.");
+      throw new Error('staticDomain is required using NextKit.');
     }
     if (!nextConfig.assetPrefix) {
       // backup old next.config.js
-      writeFileSync(
-        path.join(ctx.fullPath, "next.config.js.bak"),
-        readFileSync(nextConfigPath)
-      );
-      console.log("patching next.config.js for CDN assets.");
+      writeFileSync(path.join(ctx.fullPath, 'next.config.js.bak'), readFileSync(nextConfigPath));
+      console.log('patching next.config.js for CDN assets.');
       nextConfig.assetPrefix = ctx.staticDomain;
-      writeFileSync(
-        nextConfigPath,
-        `module.exports = ${JSON.stringify(nextConfig)}`
-      );
+      writeFileSync(nextConfigPath, `module.exports = ${JSON.stringify(nextConfig)}`);
     }
     const execp = promisify(exec);
     const result = await execp(`cd ${ctx.fullPath} && npm run build`);
@@ -39,25 +34,29 @@ export class NextKit extends Kit {
     }
     console.log(result.stdout);
     // restore user next.config.js
-    if (existsSync(path.join(ctx.fullPath, "next.config.js.bak"))) {
-      writeFileSync(
-        nextConfigPath,
-        readFileSync(path.join(ctx.fullPath, "next.config.js.bak"))
-      );
-      unlinkSync(path.join(ctx.fullPath, "next.config.js.bak"));
+    if (existsSync(path.join(ctx.fullPath, 'next.config.js.bak'))) {
+      writeFileSync(nextConfigPath, readFileSync(path.join(ctx.fullPath, 'next.config.js.bak')));
+      unlinkSync(path.join(ctx.fullPath, 'next.config.js.bak'));
     }
     // execute runkit
     const runKit = new RunKit();
     runKit.detect(ctx);
     const runKitResult = await runKit.run(ctx, {
-      fileGlob: ["package*.json", ".next/**/*", "Dockerfile"],
+      fileGlob: ['package*.json', '.next/**/*'],
+      providedFile: {
+        Dockerfile: `FROM node:alpine
+COPY . /app
+WORKDIR /app
+RUN npm ci
+ENTRYPOINT [ "npm", "start" ]`
+      }
     });
     return {
       ...runKitResult,
       staticTarget: {
-        public: "",
-        ".next/static": "_next/static/",
-      },
+        public: '',
+        '.next/static': '_next/static/'
+      }
     };
   }
 }
--- packages/cloudkit/src/kits/runkit.ts
+++ packages/cloudkit/src/kits/runkit.ts
@@ -1,43 +1,45 @@
-import { Kit, KitType, IKitContext, IKitDeployTarget } from "../common/kit";
-import archiver from "archiver";
-import { createWriteStream } from "node:fs";
-import path from "node:path";
+import { Kit, KitType, IKitContext, IKitDeployTarget } from '../common/kit';
+import archiver from 'archiver';
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
 
 export interface IRunKitOptions {
   /**
    * RunKit file filter
    */
   fileGlob?: string[];
+  providedFile?: Record<string, string>;
 }
 
 export class RunKit extends Kit {
   static type = KitType.RUN;
-  static description = "RunKit";
-  async run(
-    ctx: IKitContext,
-    options?: IRunKitOptions
-  ): Promise<IKitDeployTarget> {
+  static description = 'RunKit';
+  async run(ctx: IKitContext, options?: IRunKitOptions): Promise<IKitDeployTarget> {
     // determine which file to be packed
     const zipName = `.temp-runkit-${Date.now()}.zip`;
-    const archive = archiver("zip");
+    const archive = archiver('zip');
     const output = createWriteStream(path.join(ctx.fullPath, zipName));
     if (options?.fileGlob) {
-      options?.fileGlob?.forEach((fg) =>
-        archive.glob(fg, { cwd: ctx.fullPath })
-      );
+      options?.fileGlob?.forEach(fg => archive.glob(fg, { cwd: ctx.fullPath }));
     } else {
       archive.directory(ctx.fullPath, false);
     }
+    // add provided file
+    if (options?.providedFile) {
+      Object.keys(options.providedFile).forEach(key => {
+        archive.append(options.providedFile![key], { name: key });
+      });
+    }
     archive.pipe(output);
-    console.log("[ ] packing files...");
+    console.log('[ ] packing files...');
     await new Promise<void>((resolve, reject) => {
-      archive.on("error", (err) => reject(err));
-      archive.on("end", () => resolve());
+      archive.on('error', err => reject(err));
+      archive.on('end', () => resolve());
       archive.finalize();
     });
-    console.log("[+] packing files done");
+    console.log('[+] packing files done');
     return {
-      runTarget: zipName,
+      runTarget: zipName
     };
   }
   async detect(ctx: IKitContext) {
