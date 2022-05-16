4ef2932b31683a53ac1f3d043aaeeb09cc2991e4
PiEgg <marksz@teamsz.xyz>
2022-05-16 17:17:31 +0800

    feat(debug): change error log to debug log

--- packages/cloudkit/src/utils/safeRequire.ts
+++ packages/cloudkit/src/utils/safeRequire.ts
@@ -1,10 +1,11 @@
+import { logger } from './debug';
 import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 export function safeRequire(filePath: string) {
   if (existsSync(filePath)) {
     try {
       return require(filePath);
     } catch (e) {
-      console.error(e.message);
+      logger.debug(e);
       if (e?.message?.includes(`Unexpected token 'export'`)) {
         let content = readFileSync(filePath, 'utf-8');
         if (content.startsWith('export default')) {
