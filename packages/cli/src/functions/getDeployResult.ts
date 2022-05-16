40894e2a74470efbab450577bb67048937336ef0
haotianlong <haotianlong@tencent.com>
2022-05-16 15:07:20 +0800

    feature(cli): use logupdate

--- packages/cli/src/functions/getDeployResult.ts
+++ packages/cli/src/functions/getDeployResult.ts
@@ -1,5 +1,6 @@
 import { DescribeServerManageTask, DescribeCloudBaseRunServer } from '../api';
 import { computedTaskLog, computedBuildLog } from '../utils/run';
+import logUpdate from 'log-update';
 
 export async function getDeployResult({
   envId,
@@ -14,6 +15,7 @@ export async function getDeployResult({
 }) {
   return new Promise<void>(resolve => {
     const timer = setInterval(async () => {
+      
       const { Task: manageTask } = await DescribeServerManageTask({
         EnvId: envId,
         ServerName: serviceName
@@ -30,7 +32,7 @@ export async function getDeployResult({
         });
         const taskLog = await computedTaskLog(envId, manageTask);
         const buildLog = await computedBuildLog(envId, versionItem);
-        log(`${taskLog}\n${buildLog}`);
+        logUpdate(`${taskLog}\n${buildLog}`);
         if (manageTask?.Status === 'finished') {
           clearInterval(timer);
           resolve();
