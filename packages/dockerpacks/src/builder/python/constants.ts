1f5832d8cb3e2badc583f2ed3792a191db7e88da
darkyzhou <darkyoooooo@gmail.com>
2022-05-18 15:17:49 +0800

    feat(dockerpacks/python): use tencent mirrors

--- packages/dockerpacks/src/builder/python/constants.ts
+++ packages/dockerpacks/src/builder/python/constants.ts
@@ -1,2 +1,5 @@
+export const TENCENT_MIRRORS_HOST = 'mirrors.cloud.tencent.com';
+export const TENCENT_INDEX_URL = 'http://mirrors.cloud.tencent.com/pypi/simple';
+
 export const REQUIREMENTS_TXT = 'requirements.txt';
 export const MANAGE_PY = 'manage.py';
--- packages/dockerpacks/src/builder/python/pip.ts
+++ packages/dockerpacks/src/builder/python/pip.ts
@@ -1,5 +1,5 @@
 import { Builder } from '@builder/builder';
-import { REQUIREMENTS_TXT } from './constants';
+import { REQUIREMENTS_TXT, TENCENT_INDEX_URL, TENCENT_MIRRORS_HOST } from './constants';
 
 export const pipBuilder: Builder = {
   async detect(ctx) {
@@ -10,6 +10,13 @@ export const pipBuilder: Builder = {
   },
   async build() {
     return dockerfile => {
+      dockerfile
+        .run(
+          ['python3', '-m', 'pip', 'config', 'set', 'global.trusted-host', TENCENT_MIRRORS_HOST],
+          ['python3', '-m', 'pip', 'config', 'set', 'global.index-url', TENCENT_INDEX_URL]
+        )
+        .comment('使用速度更快的国内镜像');
+
       dockerfile
         .copy(REQUIREMENTS_TXT, REQUIREMENTS_TXT)
         .comment(`将 ${REQUIREMENTS_TXT} 复制到容器中`);
