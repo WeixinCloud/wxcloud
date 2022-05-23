08f8606c92e8c4f6f283384e17024d1bfc4d81fb
darkyzhou <darkyoooooo@gmail.com>
2022-05-23 11:47:12 +0800

    test(dockerpacks): fix express fixture

--- packages/dockerpacks/test/node/fixtures/expressjs/src/index.js
+++ packages/dockerpacks/test/node/fixtures/expressjs/src/index.js
@@ -1,5 +1,5 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
