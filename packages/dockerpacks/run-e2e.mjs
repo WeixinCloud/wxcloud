99f43ed11fc58a73d813ce1b3d7bf0872a4f9b05
darkyzhou <darkyoooooo@gmail.com>
2022-05-23 11:18:03 +0800

    test(dockerpacks/run-e2e.mjs): fix an async bug

--- packages/dockerpacks/run-e2e.mjs
+++ packages/dockerpacks/run-e2e.mjs
@@ -2,6 +2,69 @@ import path from 'path';
 import glob from 'fast-glob';
 import { execSync } from 'child_process';
 import { existsSync, readFileSync } from 'fs';
+import { isPromise } from 'util/types';
+
+const FIXTURES = glob.sync('./test/**/fixtures/*', {
+  onlyFiles: false,
+  onlyDirectories: true,
+  absolute: true
+});
+
+main().catch(console.error);
+
+async function main() {
+  for (const fixturePath of FIXTURES) {
+    process.chdir(fixturePath);
+
+    const name = path.basename(fixturePath);
+    const imageName = `dockerpacks-test-${name}`;
+
+    log`=== CASE ${name} ===`;
+    if (existsSync('.__skip__')) {
+      log`=== SKIPPED: .__skip__ presents ===\n\n`;
+      continue;
+    }
+    if (!existsSync('Dockerfile')) {
+      log`=== SKIPPED: missing Dockerfile ===\n\n`;
+      continue;
+    }
+
+    try {
+      log`* Building image ${imageName}`;
+      $`docker build .  -t ${imageName}`;
+
+      if (existsSync('.__build__only__')) {
+        log`skipped running stage, .__build__only__ presents`;
+        continue;
+      }
+
+      const content = readFileSync('Dockerfile', 'utf8');
+      const port = [...content.matchAll(/^EXPOSE (.*)$/gim)][0][1];
+      if (!port) {
+        throw new Error('missing EXPOSE in Dockerfile');
+      }
+
+      const containerName = `test-container-${Math.random().toString(36).slice(2)}`;
+      log`* Starting container ${containerName}`;
+      await attempt(
+        'Failed to start container',
+        () => $`docker run -d -p 23333:${port} --name '${containerName}' '${imageName}' > /dev/null`
+      );
+
+      log`* Checking readiness on port ${port}`;
+      await attempt(
+        'Service failed to response',
+        () => retry(() => $`curl -s --show-error localhost:23333 -o /dev/null`, 30, 3),
+        () => $`docker rm --force '${containerName}' > /dev/null`
+      );
+
+      log`=== PASSED ===\n\n`;
+    } catch (e) {
+      log`=== FAILED ===`;
+      throw e;
+    }
+  }
+}
 
 const concat = (strings, ...args) => {
   let result = '';
@@ -12,68 +75,12 @@ const concat = (strings, ...args) => {
 };
 const $ = (strings, ...args) => execSync(concat(strings, ...args), { stdio: 'inherit' });
 const log = (strings, ...args) => console.log(concat(strings, ...args));
-
-const fixtures = glob.sync('./test/**/fixtures/*', {
-  onlyFiles: false,
-  onlyDirectories: true,
-  absolute: true
-});
-
-for (const fixturePath of fixtures) {
-  process.chdir(fixturePath);
-
-  const name = path.basename(fixturePath);
-  const imageName = `dockerpacks-test-${name}`;
-
-  log`=== CASE ${name} ===`;
-  if (existsSync('.__skip__')) {
-    log`=== SKIPPED: .__skip__ presents ===\n\n`;
-    continue;
-  }
-  if (!existsSync('Dockerfile')) {
-    log`=== SKIPPED: missing Dockerfile ===\n\n`;
-    continue;
-  }
-
+const attempt = async (message, action, final) => {
   try {
-    log`* Building image ${imageName}`;
-    $`docker build .  -t ${imageName}`;
-
-    if (existsSync('.__build__only__')) {
-      log`skipped running stage, .__build__only__ presents`;
-      continue;
+    let value = action();
+    if (isPromise(value)) {
+      await value;
     }
-
-    const content = readFileSync('Dockerfile', 'utf8');
-    const port = [...content.matchAll(/^EXPOSE (.*)$/gim)][0][1];
-    if (!port) {
-      throw new Error('missing EXPOSE in Dockerfile');
-    }
-
-    const containerName = `test-container-${Math.random().toString(36).slice(2)}`;
-    log`* Starting container ${containerName}`;
-    attempt(
-      'Failed to start container',
-      () => $`docker run -d -p 23333:${port} --name '${containerName}' '${imageName}' > /dev/null`
-    );
-
-    log`* Checking readiness on port ${port}`;
-    attempt(
-      'Service failed to response',
-      () => retry(() => $`curl -s --show-error localhost:23333 -o /dev/null`, 30, 3),
-      () => $`docker rm --force '${containerName}' > /dev/null`
-    );
-
-    log`=== PASSED ===\n\n`;
-  } catch (e) {
-    log`=== FAILED ===`;
-    throw e;
-  }
-}
-
-function attempt(message, action, final) {
-  try {
-    action();
   } catch (e) {
     throw new Error(`${message}: ${e.message}`);
   } finally {
@@ -81,9 +88,8 @@ function attempt(message, action, final) {
       final?.();
     } catch {}
   }
-}
-
-async function retry(action, maxTimes, delay) {
+};
+const retry = async (action, maxTimes, delay) => {
   let time = 0;
   while (true) {
     try {
@@ -97,4 +103,4 @@ async function retry(action, maxTimes, delay) {
       await new Promise(resolve => void setTimeout(resolve, delay));
     }
   }
-}
+};
