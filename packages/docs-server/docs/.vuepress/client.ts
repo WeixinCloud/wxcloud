2ee32357825f8fab423cb00d9e4977a14a94dfa0
haotianlong <haotianlong@tencent.com>
2022-06-06 11:05:56 +0800

    fix(docs): dont import windi in client

--- packages/docs-server/docs/.vuepress/client.ts
+++
@@ -1,8 +0,0 @@
-import 'virtual:windi.css'
-import { defineClientConfig } from '@vuepress/client'
-
-export default defineClientConfig({
-  enhance({ app, router, siteData }){},
-  setup(){},
-  rootComponents: [],
-})
