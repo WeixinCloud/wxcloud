9f6c4499bd5fb13bf15035ba51c69bfd2f48e46e
darkyzhou <darkyoooooo@gmail.com>
2022-06-09 15:45:22 +0800

    test(cli): add more tests

---
+++ packages/cli/test/constants.ts
@@ -0,0 +1,5 @@
+export const TEST_APP_ID = 'wx069a87eae381af2b';
+export const TEST_APP_KEY =
+  'AAQ9G7sEAAABAAAAAAA0wOtj8MKuor6wMcSdYiAAAAAraAdzKm8i8JwFJ68cDOJBtIHsmv3F8e00LCv7f+tYvtRKJNWjemEG9Y1/xbF6qhJ5puOwHq/xJi19Q19HXUtyp/RoO4Gjp0D4ZOC7lWYnVzO7pfJnQkvu0Sx/DRksakIkq0tW/O5PZJNgHxs7yh9dEa0txgO0djr1Tg==';
+export const TEST_ENV_ID = 'test-haotian-6g03cruyb4101d37';
+export const TEST_SERVICE_ID = `test-service-${Math.round(Math.random() * 1000000)}`;
