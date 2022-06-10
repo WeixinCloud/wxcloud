302da4e51feea0f655eba10df09dd8b7f11babab
darkyzhou <darkyoooooo@gmail.com>
2022-06-10 15:15:04 +0800

    feat(docs-server): add VersionSince badge

--- packages/docs-server/docs/.vuepress/windi.config.ts
+++ packages/docs-server/docs/.vuepress/windi.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'windicss/helpers';
 export default defineConfig({
   preflight: false,
   extract: {
-    include: ['*/**/HomeLayout.vue'],
+    include: ['*/**/*.vue'],
     exclude: [
       'node_modules',
       '.git',
@@ -19,8 +19,9 @@ export default defineConfig({
     btn: 'rounded border border-gray-300 text-gray-600 px-4 py-2 m-2 inline-block hover:shadow',
     'framework-card':
       'max-w-200px max-h-120px bg-green-400 rounded-12px border-solid border-1 border-green-300 grid place-items-center',
-    'primary-button': 'bg-green-500 text-base w-184px h-48px text-white rounded-4px cursor-pointer grid place-items-center',
-    'section': 'mx-auto px-24px sm:px-64px lg:px-128px xl:px-256px'
+    'primary-button':
+      'bg-green-500 text-base w-184px h-48px text-white rounded-4px cursor-pointer grid place-items-center',
+    section: 'mx-auto px-24px sm:px-64px lg:px-128px xl:px-256px'
   },
   theme: {
     extend: {
