8ff3b715667927456d33189c40bdb2ff7b96f73f
haotianlong <haotianlong@tencent.com>
2022-06-29 11:50:37 +0800

    fix(cli): dont ignore dockerfile

--- packages/cli/src/functions/getDockerIgnore.ts
+++ packages/cli/src/functions/getDockerIgnore.ts
@@ -9,6 +9,8 @@ export function getDockerIgnore(cwd: string) {
   if (fs.existsSync(dockerIgnore)) {
     console.log(chalk.blue(`读取到 .dockerignore, 将忽略其中的文件`));
     fileToIgnore = parse(dockerIgnore);
+    // keep dockerfile since we need that to deploy
+    fileToIgnore = fileToIgnore.filter(f => f !== 'Dockerfile' && f !== 'Dockerfile*');
   }
   return fileToIgnore;
 }
